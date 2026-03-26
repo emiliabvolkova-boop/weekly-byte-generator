fa3fc68b8cb984f1083deab07166a0e159496b50
emiliabvolkova-boop <emiliabvolkova@gmail.com>
2026-03-27 00:34:07 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,10 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
-export default defineConfig({})
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html')
+    }
+  }
+})
